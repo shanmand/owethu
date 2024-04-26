07dda41b2f5caa3b3abb222660cb1ed6dd659cf5
shanmand <49024314+shanmand@users.noreply.github.com>
2024-04-26 21:24:55 +0200

    Add files via upload

---
+++ contact_us.php
@@ -0,0 +1,194 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta name="description" content="Do you have difficulties with hearing or speech? Do you suffer from painful discharging and/or ringing ears.">
+<meta name="keywords" content="audiologist, lister, brenthurst, rand hospital, netcare, hearing, ear, speech, nose, johannesburg, Katijah, Khoza-Shangase, Khoza, ear doctor, speech doctor, ear shout africa, speech south africa, ear johannesburg, speech johannesburg, ringing ears, stammer, stutter, tinnitus">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=9" />
+<title>Speech and Hearing Practice: Contact Us</title>
+<link rel="stylesheet" href="css/main1.css" type="text/css" media="screen"/>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
+</script><![endif]-->
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="SpryAssets/SpryValidationTextarea.js" type="text/javascript"></script>
+<script type="text/javascript">
+<!-- 
+var image1=new Image()
+image1.src="images/image1.jpg"
+var image2=new Image()
+image2.src="images/image2.jpg"
+var image3=new Image()
+image3.src="images/image3.jpg"
+var image4=new Image()
+image4.src="images/image4.jpg"
+var image5=new Image()
+image5.src="images/image5.jpg"
+var image5=new Image()
+image5.src="images/image5.jpg"
+var image6=new Image()
+image6.src="images/image6.jpg"
+var image7=new Image()
+image7.src="images/image7.jpg"
+var image8=new Image()
+image8.src="images/image8.jpg"
+var image9=new Image()
+image9.src="images/image9.jpg"
+var image10=new Image()
+image10.src="images/image10.jpg"
+var image11=new Image()
+image11.src="images/image11.jpg"
+var image12=new Image()
+image12.src="images/image12.jpg"
+var image13=new Image()
+image13.src="images/image13.jpg"
+
+//-->
+</script>
+<link rel="shortcut icon" href="http://www.eardoctor.co.za/favicon.ico">
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
+<link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="wrapper">
+
+
+	
+    <div id="navbar">
+      <img src="images/topbar.jpg" name="topbar" width="815" height="118"/>
+    </div>
+<div id="contact_header">
+      <div id="webpage">
+            <a href="index.html">www.eardoctor.co.za</a>
+          Tel: <a href="tel:0823390605">082 339 0605</a> email: <a href="mailto:info@eardoctor.co.za">info@eardoctor.co.za</a>
+      </div>
+    </div>	
+    <nav>
+    	<div class="menu">
+        	<ul>
+            	<li><a href="index.html">Home</a></li>
+                <li><a href="about_us.html">About Us</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="education.html">Education</a></li>
+            	<li><a href="contact_us.html" id="current">Contact Us</a></li>
+                
+                
+                
+                
+            </ul>
+        </div>
+    </nav>
+        
+  <div id="slidewrapper">
+      <img src="images/image1.jpg" name="slide" width="815" height="350"/>
+  </div>
+
+	
+  <script>
+          <!--
+          var step=1
+          function slideit(){
+              if (!document.images)
+              return
+              document.images.slide.src=eval("image"+step+".src")
+              if (step<13)
+              step++
+              else
+              step=1
+              setTimeout("slideit()",5000)
+          }
+          slideit()
+          //-->
+  </script>
+      <h1 id="currentpg_h1">Send us a message</h1>
+
+	
+
+
+<form method="POST" name="contact_form"
+action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+<label for="name">Name: </label>
+<input type="text" name="name"
+value="<?php echo htmlentities($name) ?>">
+<label for="email">Email: </label>
+<input type="text" name="email"
+value="<?php echo htmlentities($visitor_email) ?>">
+<label for="message">Message:</label>
+<textarea name="message" rows=8 cols=30
+><?php echo htmlentities($user_message) ?></textarea>
+<img src="captcha_code_file.php?rand=<?php echo rand(); ?>"
+id="captchaimg" >
+<label for="message">Enter the code above here :</label>
+<input id="6_letters_code" name="6_letters_code" type="text">
+<input type="submit" value="Submit" name="submit">
+</form>
+
+	<p>&nbsp;</p>
+    <hr/>
+    <h1 id="currentpg_h1">Where to find us?</h1>
+      <p>Our practice is located at the following locations:</p>
+    
+    
+	<div id="contact_info">
+    
+       
+      <section id="contactaddress">
+        	<h2>Lister Medical Towers</h2>
+            <address>Room 602B, 6th Floor<br/>
+            195 Jeppe Street, Johannesburg<br/><br/><br/>
+            
+            <pre>Booking:<a href="tel:0113330011">011 333 0011</a></pre>
+            <pre> 	<a href="tel:0113371430">011 337 1430</a></pre>
+            <pre>Fax:  	<a href="tel:0865536055">086 553 6055</a></pre>
+            <pre>Cell:	<a href="tel:0823390605">082 339 0605</a></pre><br/>
+            </address><br/>
+             
+        </section>
+   <section id="contactaddress">
+        	<h2>Life Brenthurst Clinic</h2>
+            <address>Sessional Consulting Rooms: <br/>Phoenix Place<br/>
+            4 Park Lane Avenue, Parktown, JHB<br/><br/>
+            
+            <pre>Booking:<a href="tel:0117174565">011 717 4565</a></pre><br/>
+            <pre>Fax: 	<a href="tel:0865536055">086 553 6055</a></pre>
+            <pre>Cell:	<a href="tel:0823390605">082 339 0605</a></pre><br/>
+            
+            </address><br/>
+             
+        </section>
+  
+  <section id="contactaddress">
+        	<h2>Netcare Rand Hospital</h2>
+            <address>Sessional Consulting Rooms<br/>
+            33 Bruce Street, Berea, JHB<br/><br/><br/>
+            
+            <pre>Booking:<a href="tel:0117174565">011 717 4565</a></pre><br/>
+            <pre>Fax: 	<a href="tel:08605536055">086 553 6055</a></pre>
+            <pre>Cell:	<a href="tel:0823390605">082 339 0605</a></pre><br/>
+            </address><br/>
+             
+        </section>      
+      <script src="//www.gmodules.com/ig/ifr?url=http://hosting.gmodules.com/ig/gadgets/file/114281111391296844949/driving-directions.xml&amp;up_fromLocation=&amp;up_myLocations=Select%20a%20clinic%20%7CLister%20Medical%20Towers%2C%20195%20Jeppe%20Street%2C%20Johannesburg%2C%20South%20Africa%20%7CLife%20Brenthurst%20Clinic%2C%204%20Park%20Ln%2C%20Parktown%2C%20Johannesburg%2C%20City%20of%20Johannesburg%2C%20Gauteng%202193%2C%20South%20Africa%20%7CNetcare%20Rand%20Hospital%2C%2033%20Bruce%20Street%2C%20Johannesburg%2C%20South%20Africa&amp;up_defaultDirectionsType=&amp;up_autoExpand=&amp;synd=open&amp;w=500&amp;h=50&amp;title=Directions+by+Google+Maps&amp;brand=light&amp;lang=en&amp;country=US&amp;border=%23ffffff%7C3px%2C1px+solid+%23999999&amp;output=js"></script>
+     
+       
+  </div>
+ 
+  	<div id="images_bottom">
+    <img src="images/bottom.jpg" />
+    </div>
+   <footer>
+   <nav id="footer_nav">
+   		<p><a href="index.html">Home</a> | <a href="about_us.html">About Us</a> | <a href="services.html">Services</a> | <a href="education.html">Education</a> | <a href="contact_us.html">Contact Us</a></p>
+   </nav>
+   <p>&copy;<script type="text/javascript">document.write(new Date().getFullYear())</script> Katijah Khoza-Shangase. All rights reserved.</p>
+   </footer>
+    
+</div>
+<script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("nameTextField", "none", {hint:"Name and Surname", minChars:5, maxChars:50, validateOn:["blur"]});
+var sprytextfield2 = new Spry.Widget.ValidationTextField("emailTextField", "email", {hint:"Your Email", validateOn:["blur", "change"]});
+var sprytextarea1 = new Spry.Widget.ValidationTextarea("messageTextArea", {maxChars:500, counterType:"chars_remaining", counterId:"countmessageTextArea", minChars:1, validateOn:["blur"]});
+var sprytextfield3 = new Spry.Widget.ValidationTextField("telephoneTextArea", "phone_number", {hint:"Enter Your 10 Digit Phone Number (NO SPACES)", format:"phone_custom", pattern:"0000000000", validateOn:["blur"]});
+</script>
+</body>
+</html>
